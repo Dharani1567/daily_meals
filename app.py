f6c3f0efc581083ec70a7439f53909d361ec8a26
Dharani1567 <dharanikan1567parthi@gmail.com>
2026-02-23 00:19:35 +0530

    completed

--- app.py
+++ app.py
@@ -10,35 +10,64 @@ scope = [
 ]
 
 creds = Credentials.from_service_account_file(
-    "credentials.json",  # your JSON file
+    "credentials.json",
     scopes=scope
 )
 
 client = gspread.authorize(creds)
 
-sheet = client.open("MealsTracker").sheet1  # Sheet name
+sheet = client.open("MealsTracker").sheet1
 
+# ---- AUTO CREATE TABLE ----
+existing_data = sheet.get_all_values()
+
+if not existing_data:
+    sheet.append_row([
+        "Type of Meal",
+        "Cost of One Plate",
+        "Total Plates",
+        "Total Cost",
+        "Company",
+        "Date"
+    ])
 
 # ---- UI ----
 st.title("Meals Entry")
 
-meal_type = st.selectbox(
-    "Type of Meal",
-    ["Breakfast", "Lunch", "Dinner", "Snacks"]
-)
+meal_type = st.text_input("Type of Meal")
 
-cost = st.number_input("Cost", min_value=0)
+one_plate_cost = st.number_input("Cost of One Plate", min_value=0)
 
-num_meals = st.number_input("Number of Meals", min_value=1)
+total_plates = st.number_input("Total Plates", min_value=1)
 
 company = st.text_input("Company")
 
+date = st.date_input("Date")
+
+# ---- Auto Calculation ----
+total_cost = one_plate_cost * total_plates
+
+st.write("### Total Cost:", total_cost)
+
+# ---- Save Button ----
 if st.button("Save Entry"):
 
-    date = datetime.now().strftime("%Y-%m-%d")
+    if meal_type.strip() == "":
+        st.error("Meal Type required")
+    elif one_plate_cost <= 0:
+        st.error("Cost must be greater than zero")
+    elif company.strip() == "":
+        st.error("Company required")
+    else:
+        row = [
+            meal_type,
+            one_plate_cost,
+            total_plates,
+            total_cost,
+            company,
+            str(date)
+        ]
 
-    row = [meal_type, cost, num_meals, company, date]
+        sheet.append_row(row)
 
-    sheet.append_row(row)
-
-    st.success("Entry Saved ✅")
+        st.success("Entry Saved ✅")
